fe083d9608f4d4218d1e9af744e327ddb2852fda
uminskiM <marcinuminski1998@gmail.com>
2020-05-14 17:52:09 +0200

    Add default view

--- orlikclient/urls.py
+++ orlikclient/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('register_or_login/', views.register_or_login_view, name='register_or_login_view'),
     path('addUser/', views.register_user, name='registerUser'),
     path('loginUser/', views.login_user, name='loginUser'),
-    path('home/', views.home, name='home'),
     path('registerFailure/', views.register_failure, name='registerFailure'),
     path('loginFailure/', views.login_failure, name='loginFailure'),
     path('systemFailure/', views.system_failure, name='systemFailure'),
